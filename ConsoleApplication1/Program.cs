addb5d4fd11155c94caaeee4874a96a3d73ef612
rojasd <donacianorojas@gmail.com>
2014-01-24 15:01:13 -0600

    aasdad

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("This was added, my first Change in GitHub");
-            Console.WriteLine("Added in Branch"); 
+            Console.WriteLine("Added in Branch");  
         }
     }
 }
