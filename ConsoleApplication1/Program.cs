ff6e6152529b0897434be29c321f06f1bd2fb40a
rojasd <donacianorojas@gmail.com>
2014-01-24 13:13:49 -0600

    Changes

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-           
+            Console.WriteLine("This was added, my first Change in GitHub");
         }
     }
 }
